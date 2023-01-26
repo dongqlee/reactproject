4fa73fffe5a7fe8609e2293a6b294c62b8d165fb
DG_LEE <dongq0513@gmail.com>
2023-01-26 18:22:02 +0900

    1822

--- src/pages/Men.js
+++ src/pages/Men.js
@@ -1,7 +1,18 @@
+import { useState } from "react"
+import prodata from "./Prodata"
 export default function Men() {
+  const prodata = useState(prodata)
+
   return (
     <>
-    
+    {
+      prodata.map((prodata, i) => {
+        return (
+          
+        )
+      })
+    }
+      
     </>
   )
 }
